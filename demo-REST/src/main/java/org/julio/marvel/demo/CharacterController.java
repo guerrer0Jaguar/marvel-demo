e56fef96133127f2a579d474bef250344a9f0164
Julio César Ramírez <juliocesar.rzg@gmail.com>
2023-10-11 16:41:11 -0600

    chore: initial import

---
+++ demo-REST/src/main/java/org/julio/marvel/demo/CharacterController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.julio.marvel.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ */
+@RestController
+public class CharacterController {
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
