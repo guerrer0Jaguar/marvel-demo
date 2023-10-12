37b216742355a1a5162c49b672700829112d6f63
Julio César Ramírez <juliocesar.rzg@gmail.com>
2023-10-12 17:34:53 -0600

    refactor: Se mueven los valores de conexión de marvel al archivo de configuracion

--- demo-REST/src/main/java/org/julio/marvel/demo/CharacterController.java
+++ demo-REST/src/main/java/org/julio/marvel/demo/CharacterController.java
@@ -7,12 +7,11 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.Date;
 
-import org.julio.marvel.demo.model.CharacterDataContainer;
 import org.julio.marvel.demo.model.CharacterDataWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,24 +25,31 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 public class CharacterController {
 	
-	private static final String PRIVATE_KEY = "c71010c21b9916d6b21920246a95349be1c38530";
-	private static final String PUBLIC_KEY = "2dd36343ecfb10400698ba276fac68fa";
+	@Value("${marvel.api.characters.url}")
+	private String marvelAPIurl;	
+	@Value("${marvel.api.key.private}")
+	private String privateKey;
+	@Value("${marvel.api.key.public}")
+	private String publicKey;
+	@Value("${marvel.api.key}")
+	private String apiKey;
+	
 	@Autowired
 	RestTemplate restTemplate;
 	
 	@GetMapping("/characters")
-	public CharacterDataWrapper characters(@RequestParam(value = "characterId", defaultValue = "World") String name) throws RestClientException, NoSuchAlgorithmException {	
+	public CharacterDataWrapper characters(@RequestParam(value = "characterId", defaultValue = "1011334") String name)
+			throws RestClientException, NoSuchAlgorithmException {	
 		return restTemplate.getForObject(buildMarvelURI(),
 				CharacterDataWrapper.class);
-		//return new CharacterDataWrapper(0, "OK","none","none","",dummyContainer(), "etag");
 	}
 
 	private String buildMarvelURI() throws NoSuchAlgorithmException {
 		Long timestamp = new Date().getTime();
-		return UriComponentsBuilder.fromHttpUrl("https://gateway.marvel.com:443/v1/public/characters")
-				.queryParam("apikey", "2dd36343ecfb10400698ba276fac68fa")
+		return UriComponentsBuilder.fromHttpUrl(marvelAPIurl)
+				.queryParam("apikey", apiKey)
 				.queryParam("ts", timestamp.toString())
-				.queryParam("hash", generateHash(timestamp.toString(),PRIVATE_KEY, PUBLIC_KEY))
+				.queryParam("hash", generateHash(timestamp.toString(),privateKey, publicKey))
 				.toUriString();		
 	}
 
@@ -57,8 +63,4 @@ public class CharacterController {
 		return String.format("%032x", new BigInteger(1, digest));
 	}
 
-	private CharacterDataContainer dummyContainer() {		
-		return new CharacterDataContainer(0, 0, 0, 
-				0, new ArrayList<org.julio.marvel.demo.model.Character>());
-	}
 }
