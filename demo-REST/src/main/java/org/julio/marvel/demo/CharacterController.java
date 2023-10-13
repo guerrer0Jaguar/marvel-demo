68caec31541527a80c4186ba591b53b791f2a19c
Julio César Ramírez <juliocesar.rzg@gmail.com>
2023-10-12 18:40:06 -0600

    feat: Se añade funcionalidad para consultar 'characters' de marvel por Id

--- demo-REST/src/main/java/org/julio/marvel/demo/CharacterController.java
+++ demo-REST/src/main/java/org/julio/marvel/demo/CharacterController.java
@@ -8,12 +8,13 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
+import java.util.Optional;
 
 import org.julio.marvel.demo.model.CharacterDataWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -37,19 +38,27 @@ public class CharacterController {
 	@Autowired
 	RestTemplate restTemplate;
 	
-	@GetMapping("/characters")
-	public CharacterDataWrapper characters(@RequestParam(value = "characterId", defaultValue = "1011334") String name)
+	@GetMapping(value= {"/characters", "/characters/{id}"})
+	public CharacterDataWrapper characters(@PathVariable Optional<Integer> characterId) 
 			throws RestClientException, NoSuchAlgorithmException {	
-		return restTemplate.getForObject(buildMarvelURI(),
+		return restTemplate.getForObject(buildMarvelRequest(characterId),
 				CharacterDataWrapper.class);
 	}
 
-	private String buildMarvelURI() throws NoSuchAlgorithmException {
+	private String buildMarvelRequest(Optional<Integer> characterId ) throws NoSuchAlgorithmException {
+		
+		UriComponentsBuilder builder = 
+		UriComponentsBuilder.fromHttpUrl(marvelAPIurl);
+		
+		if (characterId.isPresent()) {
+			builder = builder.pathSegment(characterId.get().toString());
+		}
 		Long timestamp = new Date().getTime();
-		return UriComponentsBuilder.fromHttpUrl(marvelAPIurl)
-				.queryParam("apikey", apiKey)
+		
+		return builder.queryParam("apikey", apiKey)
 				.queryParam("ts", timestamp.toString())
 				.queryParam("hash", generateHash(timestamp.toString(),privateKey, publicKey))
+				.encode()
 				.toUriString();		
 	}
 
@@ -60,6 +69,7 @@ public class CharacterController {
 		MessageDigest md = MessageDigest.getInstance("MD5");
 		md.update(toEncript.toString().getBytes(StandardCharsets.UTF_8));
 		byte [] digest = md.digest();
+		
 		return String.format("%032x", new BigInteger(1, digest));
 	}
 
