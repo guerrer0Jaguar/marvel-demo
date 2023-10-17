08b19d692d1ca88780722a3ae823346ee3add71a
Julio César Ramírez <juliocesar.rzg@gmail.com>
2023-10-17 13:00:44 -0600

    feat: Se añade link para consultar detalle del carácter de Marvel (solo request)

--- demo-web/src/main/js/app.js
+++ demo-web/src/main/js/app.js
@@ -7,6 +7,7 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {characters: []};
+		this.onDetail = this.onDetail.bind(this);
 	}
 
 	componentDidMount() { 
@@ -17,15 +18,27 @@ class App extends React.Component {
 
 	render() { 
 		return (
-			<CharacterList characters={this.state.characters}/>
+			<CharacterList 
+				characters={this.state.characters}
+				onDetail={this.onDetail}/>
 		)
+	}
+	
+	onDetail(character) {
+		let uri = 'http://localhost:8080/api/characters/' + character.id;
+		client({method: 'GET', path: uri}).done(response => {			
+			console.log(response.entity.data);
+		});
 	}		
 }
 
 class CharacterList extends React.Component{
 	render() {
 		const characters = this.props.characters.map(character =>
-			<Character key={character.id} character={character}/>
+			<Character 
+				key={character.id} 
+				character={character}
+				onDetail={this.props.onDetail}/>
 		);
 		return (
 			<table>
@@ -43,10 +56,19 @@ class CharacterList extends React.Component{
 }
 
 class Character extends React.Component{
+	constructor(props) {
+		super(props);
+		this.handleDetail = this.handleDetail.bind(this);
+	}
+
+	handleDetail() {		
+		this.props.onDetail(this.props.character);
+	}
+	
 	render() {
 		return (
 			<tr>
-				<td>{this.props.character.id}</td>
+				<td><a href="#" onClick={this.handleDetail}>{this.props.character.id} </a></td>
 				<td>{this.props.character.name}</td>
 				<td>{this.props.character.description}</td>
 			</tr>
